9eeb31addf101ee92b1ec0040c2ec6696ac8b6c7
Salem Kapic <salemko@gmail.com>
2026-09-05 16:58:48 +0200

    Connect Supabase authentication layer

---
+++ src/lib/auth-supabase.ts
@@ -0,0 +1,8 @@
+﻿import { supabase } from './supabase'
+
+export async function signInWithPassword(email: string, password: string) {
+  return supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+}
